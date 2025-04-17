72bcc500040cc95d50ae13d9bcc824c54eaa39e0
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-17 10:00:27 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="repo-34",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Experimental data project using NumPy and SQLAlchemy in Python",
+    keywords="repo-34, python",
+    url="",
+)
